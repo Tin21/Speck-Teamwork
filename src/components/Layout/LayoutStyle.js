0291eb7d00a208e53254d777d854cd6511695f25
Tin21 <tin.franic8@gmail.com>
2023-06-05 02:03:42 +0200

    Started working on Layout for desktop version

--- src/components/Layout/LayoutStyle.js
+++ src/components/Layout/LayoutStyle.js
@@ -6,6 +6,12 @@ import { ReactComponent as DropdownArrowIcon } from '../../assets/images/dropdow
 
 export const Header = styled.header`
   background-color: ${colors.bgSecondary};
+
+  @media screen and (${breakpoints.desktopSmall}) {
+    position: absolute;
+    left: 240px;
+    right: 0;
+  }
 `;
 
 export const HeaderInner = styled.div`
@@ -19,17 +25,30 @@ export const HeaderInner = styled.div`
   @media screen and (${breakpoints.tabletSmall}) {
     padding: 10px 90px 10px 70px;
   }
+
+  @media screen and (${breakpoints.desktopSmall}) {
+    padding: 10px 40px;
+  }
 `;
 
 export const HamburgerLogo = styled(HamburgerIcon)`
   display: block;
   height: 18px;
   width: 27px;
+  margin-left: 37px;
+
+  @media screen and (${breakpoints.desktopSmall}) {
+    display: none;
+  }
 `;
 
 export const LogoWrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  @media screen and (${breakpoints.desktopSmall}) {
+    display: none;
+  }
 `;
 
 export const SpeckLogo = styled(SpeckIcon)`
@@ -66,13 +85,12 @@ export const UserAvatar = styled.img`
     width: 40px;
     border-radius: 50%;
     border: 1.5px solid ${colors.avatarBorder};
-    margin-right: 8px;
   }
 `;
 
 export const DropdownArrow = styled(DropdownArrowIcon)`
   display: none;
-  margin-right: 37px;
+  margin-left: 8px;
 
   @media screen and (${breakpoints.tabletSmall}) {
     display: block;
@@ -87,4 +105,56 @@ export const AvatarHamburgerWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: right;
+`;
+
+export const HeaderText = styled.p`
+  display: none;
+
+  @media screen and (${breakpoints.desktopSmall}) {
+    display: block;
+    font-weight: 500;
+    font-size: 16px;
+  }
+`;
+
+export const Menu = styled.div`
+  display: none;
+
+  @media screen and (${breakpoints.desktopSmall}) {
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 240px;
+    background-color: ${colors.bgSecondary};
+    border-right: 1.5px solid ${colors.border};
+  }
+`;
+
+export const MenuLogoWrapper = styled.div`
+  display: none;
+
+  @media screen and (${breakpoints.desktopSmall}) {
+    display: flex;
+    flex-direction: column;
+    padding: 32px 30px;
+  }
+`;
+
+export const MenuSpeckLogo = styled.img`
+  display: none;
+  margin-bottom: 0;
+  width: 108px;
+  height: 42px;
+  @media screen and (${breakpoints.tabletSmall}) {
+    display: block;
+  }
+`;
+
+export const MenuLogoText = styled.p`
+  font-weight: 500;
+  color: ${colors.logoText};
+  letter-spacing: 0.1em;
+  font-size: 10px;
 `;
