f12f9d5c9b145048760959cde9cc814fde9973e1
Ana Skarica <ana.skarica.as@gmail.com>
2023-06-06 23:37:42 +0200

    added mock data for bar chart

---
+++ src/utils/mock/chartData.js
@@ -0,0 +1,32 @@
+export const chartMock = [
+  {
+    id: 1,
+    title: 'Introduction',
+    percentage: 100,
+  },
+  {
+    id: 2,
+    title: 'HTML & CSS',
+    percentage: 97,
+  },
+  {
+    id: 3,
+    title: 'Version Control System',
+    percentage: 92,
+  },
+  {
+    id: 4,
+    title: 'Advanced CSS',
+    percentage: 83,
+  },
+  {
+    id: 5,
+    title: 'Intro To JS',
+    percentage: 64,
+  },
+  {
+    id: 7,
+    title: 'Intro To React',
+    percentage: 52,
+  },
+];
