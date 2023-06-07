ee1fb22d9dd01737a54c45e3b77fb67509940ae3
Ana Skarica <ana.skarica.as@gmail.com>
2023-06-07 08:43:56 +0200

    add first version of horizontal bar chart

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -1,8 +1,11 @@
 import Badges from '../components/Badges/Badges';
 
+import BarChart from '../components/BarChart/BarChart';
+
 const Achievements = () => {
   return (
     <>
+    <BarChart />
       <Badges />
     </>
   );
