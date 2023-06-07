c97dcef5237382fcba0718af602057389a387bbe
Ladislav Žerjavić <ladislav.zerjavic@gmail.com>
2023-06-07 19:12:28 +0200

    removed subtitle from lecture page

--- src/pages/Lectures.jsx
+++ src/pages/Lectures.jsx
@@ -14,11 +14,7 @@ const Lectures = () => {
 
   return (
     <>
-      <Section
-        isFlexDisplay={false}
-        title={'Your lectures'}
-        subtitle={'Lorem ipsum sit dolor amet bl abla'}
-      >
+      <Section isFlexDisplay={false} title={'Your lectures'}>
         {lectures && (
           <Grid>
             {lectures.map((lecture) => (
