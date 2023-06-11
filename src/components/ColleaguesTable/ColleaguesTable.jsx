7939858c7ec9f4802faa2e153908ab7189d0e9c5
Ladislav Žerjavić <ladislav.zerjavic@gmail.com>
2023-06-11 19:32:32 +0200

    added colleagues table folder, installed tanstack

---
+++ src/components/ColleaguesTable/ColleaguesTable.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ColleaguesTable = (props) => {
+  return <div>ColleaguesTable</div>;
+};
+
+ColleaguesTable.propTypes = {};
+
+export default ColleaguesTable;
