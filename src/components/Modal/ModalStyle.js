667d4f4ab1292c147b2fe31cb8a7d2a4b8eda3e0
Tin21 <tin.franic8@gmail.com>
2023-06-13 23:59:13 +0200

    Added modal component for mobile version

--- src/components/Modal/ModalStyle.js
+++ src/components/Modal/ModalStyle.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { colors, breakpoints } from '../../utils/styles/theme';
+import { ReactComponent as ModalCancelIcon } from '../../assets/images/cancel-icon.svg';
+import { Button as GeneralButton } from '../../utils/styles/generalStyles';
 
 export const ModalBackground = styled.div`
   position: fixed;
@@ -18,5 +20,73 @@ export const ModalBackground = styled.div`
 export const ModalWrapper = styled.div`
   background-color: ${colors.white};
   width: calc(100% - 48px);
-  /* width: 272px; */
+  max-width: 340px;
+  border-radius: 4px;
+  border: 1px solid ${colors.textLightGrey};
+  box-shadow: 0px 24px 38px rgba(0, 0, 0, 0.14),
+    0px 11px 15px rgba(0, 0, 0, 0.2);
+`;
+
+export const TitleWrapper = styled.div`
+  width: 100%;
+  height: 46px;
+  padding: 20px 22px 20px 18px;
+  border-bottom: 1px solid #dedede;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TitleText = styled.p`
+  font-weight: 600;
+  font-size: 16px;
+`;
+
+export const CancelIcon = styled(ModalCancelIcon)`
+  height: 18px;
+  width: 18px;
+  path {
+    fill: ${colors.textSecondaryGrey};
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const ModalBody = styled.div`
+  padding: 28px 18px 22px 18px;
+  height: max-content;
+`;
+
+export const ModalBodyText = styled.p`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 140%;
+  color: ${colors.textSecondaryGrey};
+  margin-bottom: 32px;
+`;
+
+export const ModalButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Button = styled(GeneralButton)`
+  height: 38px;
+  padding: 0 22px;
+  width: max-content;
+
+  ${(props) =>
+    props.isYesButton &&
+    `
+      border: 1px solid ${colors.buttonBorder};
+      color: ${colors.buttonPrimary};
+      background: ${colors.white};
+
+      &:hover {
+          color: ${colors.white};
+        }
+    `}
 `;
