4f4a5a5b56c2682e96a8d4dfc3b72f81bce80cf5
Tin21 <tin.franic8@gmail.com>
2023-06-13 23:59:13 +0200

    Added modal component for mobile version

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
-import { ModalBackground, ModalWrapper } from './ModalStyle';
+import React, { useState } from 'react';
+import {
+  CancelIcon,
+  ModalBackground,
+  ModalBody,
+  ModalBodyText,
+  ModalButtonWrapper,
+  ModalWrapper,
+  TitleText,
+  TitleWrapper,
+  Button,
+} from './ModalStyle';
 
 const Modal = () => {
+  const [visible, setVisible] = useState(true);
+
+  const changeVisibility = () => {
+    setVisible(false);
+  };
+
   return (
-    <ModalBackground>
-      <ModalWrapper>bok</ModalWrapper>
-    </ModalBackground>
+    <>
+      {visible && (
+        <ModalBackground>
+          <ModalWrapper>
+            <TitleWrapper>
+              <TitleText>Delete email?</TitleText>
+              <CancelIcon onClick={() => changeVisibility()} />
+            </TitleWrapper>
+            <ModalBody>
+              <ModalBodyText>
+                If you delete email, all of the content from email will be
+                deleted.
+              </ModalBodyText>
+              <ModalButtonWrapper>
+                <Button isYesButton onClick={() => changeVisibility()}>
+                  Delete
+                </Button>
+                <Button onClick={() => changeVisibility()}>Keep editing</Button>
+              </ModalButtonWrapper>
+            </ModalBody>
+          </ModalWrapper>
+        </ModalBackground>
+      )}
+    </>
   );
 };
 
