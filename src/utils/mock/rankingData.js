07b15356e2aa6a9c1a881ea95e2bea189a9b9a94
Tin21 <tin.franic8@gmail.com>
2023-06-15 14:24:59 +0200

    Added mock rankings

---
+++ src/utils/mock/rankingData.js
@@ -0,0 +1,93 @@
+import BadgeExamSilver from '../../assets/images/badges/badge-exam-icon-silver.svg';
+import BadgeExamGold from '../../assets/images/badges/badge-exam-icon-gold.svg';
+import BadgeTaskSilver from '../../assets/images/badges/badge-task-icon-silver.svg';
+import BadgeTaskGold from '../../assets/images/badges/badge-task-icon-gold.svg';
+import BadgeTeamSilver from '../../assets/images/badges/badge-team-icon-silver.svg';
+import BadgeTeamGold from '../../assets/images/badges/badge-team-icon-gold.svg';
+import BadgeQuizSilver from '../../assets/images/badges/badge-quiz-icon-silver.svg';
+import BadgeQuizGold from '../../assets/images/badges/badge-quiz-icon-gold.svg';
+import BadgeTimeSilver from '../../assets/images/badges/badge-time-icon-silver.svg';
+import BadgeTimeGold from '../../assets/images/badges/badge-time-icon-gold.svg';
+import { SingleBadge } from '../../components/SingleBadge/SingleBadge';
+
+export const rankingMock = [
+  {
+    id: 1,
+    name: 'Adrijano Tubak',
+    points: 145,
+    percentage: '90%',
+    badges: [
+      BadgeExamSilver,
+      BadgeExamGold,
+      BadgeTaskGold,
+      BadgeTeamSilver,
+      BadgeTaskSilver,
+      BadgeQuizSilver,
+      BadgeTeamGold,
+    ],
+  },
+  {
+    id: 2,
+    name: 'Anton Majstorka',
+    points: 141,
+    percentage: '88%',
+    badges: [
+      BadgeExamSilver,
+      BadgeExamGold,
+      BadgeTaskGold,
+      BadgeQuizGold,
+      BadgeTimeGold,
+      BadgeTimeGold,
+      BadgeTeamGold,
+    ],
+  },
+  {
+    id: 3,
+    name: 'Dejan Soldo',
+    points: 132,
+    percentage: '85%',
+    badges: [
+      BadgeExamSilver,
+      BadgeExamGold,
+      BadgeTaskGold,
+      BadgeQuizGold,
+      BadgeTimeGold,
+      BadgeTeamGold,
+    ],
+  },
+  {
+    id: 4,
+    name: 'Gabrijela Leskovac',
+    points: 130,
+    percentage: '79%',
+    badges: [BadgeExamSilver, BadgeExamGold, BadgeTaskGold, BadgeQuizGold],
+  },
+  {
+    id: 5,
+    name: 'Rafael Bodrušić',
+    points: 123,
+    percentage: '77%',
+    badges: [BadgeExamSilver, BadgeExamGold, BadgeTaskGold, BadgeQuizGold],
+  },
+  {
+    id: 6,
+    name: 'Ivan Horvat',
+    points: 120,
+    percentage: '75%',
+    badges: [BadgeExamSilver, BadgeExamGold, BadgeTaskGold, BadgeQuizGold],
+  },
+  {
+    id: 7,
+    name: 'Marija Horvat',
+    points: 118,
+    percentage: '74%',
+    badges: [BadgeExamSilver, BadgeExamGold, BadgeTaskGold, BadgeQuizGold],
+  },
+  {
+    id: 8,
+    name: 'Hrvoje Horvat',
+    points: 115,
+    percentage: '72%',
+    badges: [BadgeExamSilver, BadgeExamGold, BadgeTaskGold, BadgeQuizGold],
+  },
+];
