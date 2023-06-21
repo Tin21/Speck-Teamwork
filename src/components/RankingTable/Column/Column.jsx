9dfccdfa20f04243cf47ea027c3a1fd3e32a564a
Tin21 <tin.franic8@gmail.com>
2023-06-21 16:44:37 +0200

    Connected ranking table with api

--- src/components/RankingTable/Column/Column.jsx
+++ src/components/RankingTable/Column/Column.jsx
@@ -5,22 +5,18 @@ import { ColumnBody } from './ColumnStyle';
 const columnHelper = createColumnHelper();
 
 const columns = [
-  columnHelper.accessor('id', {
+  columnHelper.accessor('ranking', {
     cell: (info) => <ColumnBody isRanking>{info.getValue()}</ColumnBody>,
     header: () => <ColumnHeader columnName="Ranking" />,
     enableSorting: false,
   }),
-
-  columnHelper.accessor('fullName', {
-    cell: (info) => {
-      const { firstName, lastName } = info.row.original;
-      const fullName = `${firstName} ${lastName}`;
-      return <ColumnBody isName>{fullName}</ColumnBody>;
-    },
+  columnHelper.accessor((row) => `${row.first_name} ${row.last_name}`, {
+    id: 'fullName',
+    cell: (info) => <ColumnBody isName>{info.getValue()}</ColumnBody>,
     header: () => <ColumnHeader asdDesc={true} columnName="Student" />,
   }),
 
-  columnHelper.accessor('points', {
+  columnHelper.accessor('pointsUser', {
     cell: (info) => <ColumnBody isPoints>{info.renderValue()}</ColumnBody>,
     header: () => <ColumnHeader asdDesc={true} columnName="Points" />,
   }),
@@ -34,7 +30,7 @@ const columns = [
     ),
   }),
 
-  columnHelper.accessor('badges', {
+  /* columnHelper.accessor('badges', {
     cell: (info) => (
       <ColumnBody>
         {info.renderValue().map((badge) => (
@@ -44,7 +40,7 @@ const columns = [
     ),
     header: () => <ColumnHeader columnName="Badges" />,
     enableSorting: false,
-  }),
+  }), */
 ];
 
 export default columns;
