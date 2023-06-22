c9df9fb4f36f396f5b1a7fd813a925ca2276b456
David Slavik <dslavik20@foi.hr>
2023-06-22 15:29:31 +0200

    Added API to login. Removed forgot password and reset password conditional rendering.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { loginUser, getUsers } from '../../api/users';
+import { useNavigate } from 'react-router-dom';
 import LoginSection from '../../components/LoginSection/LoginSection';
+import { AuthContext } from '../../context/AuthContext';
 import * as Yup from 'yup';
 
 import {
@@ -11,6 +14,7 @@ import {
   Button,
   StyledLink,
   ErrorMessage,
+  Form,
 } from '../../utils/styles/generalStyles';
 
 import { Formik } from 'formik';
@@ -18,16 +22,10 @@ import { Formik } from 'formik';
 import loginImage from '../../assets/images/login-image.jpg';
 
 const Login = () => {
+  const { setIsLoggedIn } = useContext(AuthContext);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const navigate = useNavigate();
   const [formState, setFormState] = useState('login');
-
-  const handleSubmit = (values, actions) => {
-    if (formState === 'login') {
-    } else if (formState === 'forgotPassword') {
-      setFormState('resetPassword');
-    } else if (formState === 'resetPassword') {
-    }
-    actions.setSubmitting(false);
-  };
 
   return (
     <LoginSection
@@ -41,141 +39,87 @@ const Login = () => {
       login_image={loginImage}
     >
       <Formik
-        initialValues={{}}
+        initialValues={{
+          email: '',
+          password: '',
+        }}
         validationSchema={Yup.object({
           email: Yup.string()
             .email('Email is invalid!')
             .required('Email is required!'),
           password: Yup.string().required('Password is required!'),
-          newPassword: Yup.string().required('New password is required!'),
-          confirmPassword: Yup.string()
-            .oneOf([Yup.ref('newPassword'), null], 'Passwords must match!')
-            .required('Repeat password is required!'),
         })}
-        onSubmit={handleSubmit}
+        onSubmit={async (values, { setSubmitting, resetForm }) => {
+          try {
+            const response = await loginUser(values);
+            console.log('Token: ' + response.access_token);
+            const users = await getUsers(response.access_token);
+            console.log(users);
+            const user = users.data.find((user) => user.email == values.email);
+            console.log(user);
+            setSuccessMessage({
+              error: false,
+              message: `User ${user.first_name} ${user.last_name} logged in successfully!`,
+            });
+            setTimeout(() => setSuccessMessage(null), 2000);
+            setTimeout(() => navigate('/'), 1500);
+            localStorage.setItem('jwt_token', response.access_token);
+            setIsLoggedIn(response.access_token);
+            resetForm();
+          } catch (err) {
+            setSuccessMessage({
+              error: true,
+              message: 'Failed to login!',
+            });
+          } finally {
+            setSubmitting(false);
+          }
+        }}
       >
         {(formik) => (
-          <>
-            {formState === 'login' && (
-              <>
-                <FormRow>
-                  <FieldLabelWrapper>
-                    <InputLabel>Email</InputLabel>
-                    <RequiredField> *</RequiredField>
-                  </FieldLabelWrapper>
-                  <InputField
-                    placeholder="Enter your email..."
-                    type="text"
-                    id="email"
-                    name="email"
-                    disabled={formik.isSubmitting}
-                    error={formik.errors.email && formik.touched.email ? 1 : 0}
-                  />
-                  <ErrorMessage component={'div'} name="email" />
-                </FormRow>
-                <FormRow>
-                  <FieldLabelWrapper>
-                    <InputLabel>Password</InputLabel>
-                    <RequiredField> *</RequiredField>
-                  </FieldLabelWrapper>
-                  <InputField
-                    placeholder="Enter your password..."
-                    type="password"
-                    id="password"
-                    name="password"
-                    disabled={formik.isSubmitting}
-                    error={
-                      formik.errors.password && formik.touched.password ? 1 : 0
-                    }
-                  />
-                  <ErrorMessage component={'div'} name="password" />
-                  <StyledLink onClick={() => setFormState('forgotPassword')}>
-                    Forgot your password?
-                  </StyledLink>
-                </FormRow>
-                <Button type="submit" disabled={formik.isSubmitting}>
-                  {formik.isSubmitting ? 'Processing...' : 'Login'}
-                </Button>
-              </>
-            )}
-            {formState === 'forgotPassword' && (
-              <>
-                <FormRow>
-                  <FieldLabelWrapper>
-                    <InputLabel>Email</InputLabel>
-                    <RequiredField> *</RequiredField>
-                  </FieldLabelWrapper>
-                  <InputField
-                    placeholder="Enter your email..."
-                    type="text"
-                    id="email"
-                    name="email"
-                    disabled={formik.isSubmitting}
-                    error={formik.errors.email && formik.touched.email ? 1 : 0}
-                  />
-                  <ErrorMessage component={'div'} name="email" />
-                </FormRow>
-                <FormRow>
-                  <Button
-                    type="submit"
-                    disabled={formik.isSubmitting}
-                    onClick={() => setFormState('resetPassword')}
-                  >
-                    {formik.isSubmitting ? 'Processing...' : 'Send to email'}
-                  </Button>
-                </FormRow>
-              </>
-            )}
+          <Form>
+            <FormRow>
+              <FieldLabelWrapper>
+                <InputLabel>Email</InputLabel>
+                <RequiredField> *</RequiredField>
+              </FieldLabelWrapper>
+              <InputField
+                placeholder="Enter your email..."
+                type="text"
+                id="email"
+                name="email"
+                disabled={formik.isSubmitting}
+                error={formik.errors.email && formik.touched.email ? 1 : 0}
+              />
+              <ErrorMessage component={'div'} name="email" />
+            </FormRow>
+            <FormRow>
+              <FieldLabelWrapper>
+                <InputLabel>Password</InputLabel>
+                <RequiredField> *</RequiredField>
+              </FieldLabelWrapper>
+              <InputField
+                placeholder="Enter your password..."
+                type="password"
+                id="password"
+                name="password"
+                disabled={formik.isSubmitting}
+                error={
+                  formik.errors.password && formik.touched.password ? 1 : 0
+                }
+              />
+              <ErrorMessage component={'div'} name="password" />
+              <StyledLink onClick={() => setFormState('forgotPassword')}>
+                Forgot your password?
+              </StyledLink>
+            </FormRow>
 
-            {formState === 'resetPassword' && (
-              <>
-                <FormRow>
-                  <FieldLabelWrapper>
-                    <InputLabel>New password</InputLabel>
-                    <RequiredField> *</RequiredField>
-                  </FieldLabelWrapper>
-                  <InputField
-                    placeholder="Enter your new password..."
-                    type="password"
-                    id="newPassword"
-                    name="newPassword"
-                    disabled={formik.isSubmitting}
-                    error={
-                      formik.errors.newPassword && formik.touched.newPassword
-                        ? 1
-                        : 0
-                    }
-                  />
-                  <ErrorMessage component={'div'} name="newPassword" />
-                </FormRow>
-                <FormRow>
-                  <FieldLabelWrapper>
-                    <InputLabel>Repeat password</InputLabel>
-                    <RequiredField> *</RequiredField>
-                  </FieldLabelWrapper>
-                  <InputField
-                    placeholder="Repeat your new password..."
-                    type="password"
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    disabled={formik.isSubmitting}
-                    error={
-                      formik.errors.confirmPassword &&
-                      formik.touched.confirmPassword
-                        ? 1
-                        : 0
-                    }
-                  />
-                  <ErrorMessage component={'div'} name="confirmPassword" />
-                </FormRow>
-                <FormRow>
-                  <Button type="submit" disabled={formik.isSubmitting}>
-                    {formik.isSubmitting ? 'Processing...' : 'Reset'}
-                  </Button>
-                </FormRow>
-              </>
-            )}
-          </>
+            <FormRow>
+              <Button type="submit" disabled={formik.isSubmitting}>
+                {formik.isSubmitting ? 'Processing...' : 'Login'}
+              </Button>
+            </FormRow>
+          </Form>
         )}
       </Formik>
     </LoginSection>
