5b081c398ecf5a5f8258ded7d34a731f55a90c32
Ladislav Žerjavić <ladislav.zerjavic@gmail.com>
2023-06-22 23:12:04 +0200

    added close and minmize svg, close functionality

--- src/components/Email/Email.jsx
+++ src/components/Email/Email.jsx
@@ -4,14 +4,24 @@ import {
   EmailBody,
   EmailContainer,
   EmailHeader,
+  EmailHeaderText,
+  MinCloseWrapper,
   Recipients,
   Subject,
 } from './EmailStyle';
+import { ReactComponent as MinimizeIcon } from './../../assets/images/emailIcon/minimize.svg';
+import { ReactComponent as CloseIcon } from './../../assets/images/emailIcon/close.svg';
 
-const Email = () => {
+const Email = ({ onClose }) => {
   return (
     <EmailContainer>
-      <EmailHeader>Send email</EmailHeader>
+      <EmailHeader>
+        <EmailHeaderText>Send email</EmailHeaderText>
+        <MinCloseWrapper>
+          <MinimizeIcon />
+          <CloseIcon onClick={onClose} />
+        </MinCloseWrapper>
+      </EmailHeader>
       <EmailBody>
         <Recipients>Recipients</Recipients>
         <Subject>Subject</Subject>
