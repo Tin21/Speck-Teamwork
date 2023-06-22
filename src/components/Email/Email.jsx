87ecbbf1e0fd747996e9b7977adf6a7c861237d4
Ladislav Žerjavić <ladislav.zerjavic@gmail.com>
2023-06-23 01:31:57 +0200

    added footer symbols, tablet style

--- src/components/Email/Email.jsx
+++ src/components/Email/Email.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import {
   BodyText,
+  BodyTextWrapper,
+  ButtonWrapper,
+  DeleteWrapper,
   EmailBody,
   EmailContainer,
+  EmailFooterWrapper,
   EmailHeader,
   EmailHeaderText,
   MinCloseWrapper,
+  MultipleIconWrapper,
   Recipients,
   Subject,
 } from './EmailStyle';
 import { ReactComponent as MinimizeIcon } from './../../assets/images/emailIcon/minimize.svg';
 import { ReactComponent as CloseIcon } from './../../assets/images/emailIcon/close.svg';
+import { ReactComponent as DeleteIcon } from './../../assets/images/emailIcon/trash.svg';
+import { ReactComponent as TextIcon } from './../../assets/images/emailIcon/text.svg';
+import { ReactComponent as ClipperIcon } from './../../assets/images/emailIcon/clipper.svg';
+import { ReactComponent as LinkIcon } from './../../assets/images/emailIcon/linker.svg';
+import { ReactComponent as EmojiIcon } from './../../assets/images/emailIcon/emoji.svg';
+import { ReactComponent as TriangleIcon } from './../../assets/images/emailIcon/triangle.svg';
+import { ReactComponent as PhotoIcon } from './../../assets/images/emailIcon/photo.svg';
+import { ReactComponent as DelayIcon } from './../../assets/images/emailIcon/delay.svg';
+import { ReactComponent as SignatureIcon } from './../../assets/images/emailIcon/signature.svg';
+import { Button } from '../../utils/styles/generalStyles';
 
 const Email = ({ onClose }) => {
   return (
@@ -25,7 +40,27 @@ const Email = ({ onClose }) => {
       <EmailBody>
         <Recipients>Recipients</Recipients>
         <Subject>Subject</Subject>
-        <BodyText>Body Text</BodyText>
+        <BodyTextWrapper>
+          <BodyText>Body text</BodyText>
+        </BodyTextWrapper>
+        <EmailFooterWrapper>
+          <DeleteWrapper>
+            <DeleteIcon />
+          </DeleteWrapper>
+          <MultipleIconWrapper>
+            <TextIcon />
+            <ClipperIcon />
+            <LinkIcon />
+            <EmojiIcon />
+            <TriangleIcon />
+            <PhotoIcon />
+            <DelayIcon />
+            <SignatureIcon />
+          </MultipleIconWrapper>
+          <ButtonWrapper>
+            <Button width="auto">Send Email</Button>
+          </ButtonWrapper>
+        </EmailFooterWrapper>
       </EmailBody>
     </EmailContainer>
   );
--- src/components/Email/EmailStyle.js
+++ src/components/Email/EmailStyle.js
@@ -2,23 +2,23 @@ import styled from 'styled-components';
 import { colors, breakpoints, fonts } from '../../utils/styles/theme';
 
 export const EmailContainer = styled.div`
-  position: fixed;
-  top: 25%;
-  left: 25%;
-  height: 50%;
+  position: absolute;
   width: 50%;
+  height: 620px;
+  top: 25%;
+  right: 25%;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  padding-top: 60px;
   box-sizing: border-box;
+  /* border: 1px solid #ccc; */
+  /* background-color: red; */
 `;
 
 export const EmailHeader = styled.div`
   display: flex;
   background-color: ${colors.textPrimaryGrey};
-  height: 40px;
   border-radius: 8px 8px 0px 0px;
   width: 100%;
   color: ${colors.white};
@@ -42,25 +42,61 @@ export const MinCloseWrapper = styled.div`
 `;
 
 export const EmailBody = styled.div`
-  height: 50%;
+  height: 430px;
   width: 100%;
   background-color: white;
   display: flex;
   flex-direction: column;
   padding: 0 16px;
+  border-radius: 0 0 8px 8px;
 `;
 
 export const Recipients = styled.div`
   color: ${colors.textSecondary};
   border-bottom: 1px solid #edeff1;
-  padding: 11px 0;
+  padding: 10px 0 8px 0;
 `;
+
 export const Subject = styled.div`
   color: ${colors.textSecondary};
   border-bottom: 1px solid #edeff1;
-  padding: 11px 0;
+  padding: 10px 0 8px 0;
 `;
-export const BodyText = styled.div`
+
+export const BodyTextWrapper = styled.div`
+  height: 80%;
+`;
+export const BodyText = styled.p`
+  padding: 10px 0 8px 0;
   color: ${colors.textSecondary};
-  padding: 11px 0;
+`;
+
+export const EmailFooterWrapper = styled.div`
+  display: none;
+  @media screen and (${breakpoints.tablet}) {
+    background-color: rgba(202, 202, 202, 0.2);
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+    padding: 12px 48px;
+    /* gap: 72px; */
+    border: 1px solid #ccc;
+  }
+`;
+
+export const DeleteWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 128px;
+`;
+
+export const MultipleIconWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 8px;
+  width: 252px;
 `;
