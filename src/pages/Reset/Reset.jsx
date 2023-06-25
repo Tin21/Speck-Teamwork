8f223604f2631897a34bf7efcfd27e0fb72162e5
David Slavik <dslavik20@foi.hr>
2023-06-26 00:56:06 +0200

    Added Forgot Password Sumbit function

--- src/pages/Reset/Reset.jsx
+++ src/pages/Reset/Reset.jsx
@@ -1,5 +1,22 @@
 const Reset = () => {
-  return <div>Reset</div>;
+  const forgotPasswordSubmit = async (values, { setSubmitting, resetForm }) => {
+    try {
+      const response = await forgotPassword(values.email);
+      console.log(response);
+      resetForm();
+      setErrorMessage({
+        error: false,
+        message: 'A mail for resetting your password has been sent!',
+      });
+    } catch (err) {
+      setErrorMessage({
+        error: true,
+        message: 'Failed to send a mail for resetting your password!',
+      });
+    } finally {
+      setSubmitting(false);
+    }
+  };
 };
 
 export default Reset;
