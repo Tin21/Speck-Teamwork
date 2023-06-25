8bf899dabe1d11b458357debadf687b2661efe3b
Ladislav Žerjavić <ladislav.zerjavic@gmail.com>
2023-06-25 16:14:12 +0200

    added minimzed style for tab/desk

--- src/components/ColleaguesTable/MinimizeEmail/MinimizeEmailStyle.js
+++ src/components/ColleaguesTable/MinimizeEmail/MinimizeEmailStyle.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { colors } from '../../../utils/styles/theme';
+import { breakpoints, colors } from '../../../utils/styles/theme';
 import { ReactComponent as ExpandMin } from './../../../assets/images/emailIcon/expandMin.svg';
 
 export const MinimizedBottom = styled.div`
@@ -10,12 +10,22 @@ export const MinimizedBottom = styled.div`
   background-color: ${colors.textPrimaryGrey};
   transition: height 0.3s ease;
   border-radius: 8px 8px 0 0;
+
+  @media screen and (${breakpoints.tablet}) {
+    left: auto;
+    width: 320px;
+    right: 72px;
+  }
 `;
 export const InnerWrapperMini = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 12px 16px;
+
+  @media screen and (${breakpoints.tablet}) {
+    padding: 8px 18px;
+  }
 `;
 
 export const ExpandWrapper = styled.div`
@@ -32,6 +42,18 @@ export const MinimizedText = styled.p`
   font-weight: 500;
   line-height: 120%;
   letter-spacing: -0.154px;
+
+  ${(props) =>
+    props.miniLeftRounded &&
+    `
+    display:none;
+    @media screen and (${breakpoints.tablet}) {
+      display:block;
+      padding:8px 12px;
+      border-radius:48px;
+      border:1px solid ${colors.buttonBorder}
+      }
+  `}
 `;
 
 export const ExpandedMin = styled(ExpandMin)`
