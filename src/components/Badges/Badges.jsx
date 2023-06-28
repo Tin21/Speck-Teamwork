10432d6bff9e19bb206ffa9856b2111a08d5bb52
Tin21 <tin.franic8@gmail.com>
2023-06-28 17:56:04 +0200

    Connected My badges component with api data

--- src/components/Badges/Badges.jsx
+++ src/components/Badges/Badges.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { BadgesGrid, BadgesTitle, BadgesWrapper } from './BadgesStyle';
 import { badges } from '../../utils/badges/badgesList';
 import SingleBadge from '../SingleBadge/SingleBadge';
+import { Context } from '../../context/Context';
 
 const Badges = () => {
+  const { usersTable } = useContext(Context);
+  const loggedUserId = localStorage.getItem('logged_user_id');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    usersTable && setUser(usersTable.find((user) => user.id == loggedUserId));
+  }, [usersTable]);
+
+  useEffect(() => {
+    console.log(user);
+  }, [user !== null]);
+
+  const checkBadge = (src) => {
+    var checked = false;
+    user !== null &&
+      user.badges?.map((badge) => {
+        if (badge.src === src) checked = true;
+      });
+    return checked;
+  };
+
   return (
     <BadgesWrapper>
       <BadgesTitle>My badges</BadgesTitle>
@@ -14,7 +36,7 @@ const Badges = () => {
             title={badge.title}
             imgSrc={badge.imgSrc}
             imgAlt={badge.imgAlt}
-            checked={badge.checked}
+            checked={checkBadge(badge.imgSrc)}
             isGolden={badge.isGolden}
           />
         ))}
